5bb2ad27004b739ba2b46c12f570b772064e9785
bapbodanbbang <62498196+bapbodanbbang@users.noreply.github.com>
2020-09-23 13:41:40 +0900

    [UPLOAD] 기본 index.js 파일 업로드

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const express_session = require('express-session');
+const cors = require('cors');
+const cookie_parser = require('cookie-parser');
+const body_parser = require('body-parser');
+const dotenv = require('dotenv');
+
+//.gitignore 다시 한번 보기
+
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
