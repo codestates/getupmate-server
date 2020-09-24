441109351d8a039936375791c9afe86755b1ec00
bapbodanbbang <62498196+bapbodanbbang@users.noreply.github.com>
2020-09-24 19:24:43 +0900

    test signup code

--- controller/user/signup.js
+++ controller/user/signup.js
@@ -8,26 +8,26 @@ module.exports = {
     let new_passowrd = req.body.email;
 
     // 이메일이나 닉네임이 동일한 것이 있을 때 에러!
-    const newUser = await user.findOne({
+    await user.findOne({
         where : {
             [OP.or] : [{nickname : new_nickname}, {email : new_email}]
         }
+    }).then(result => {
+        if(result !== null){
+            res.status(409);
+            res.send("already exist!")
+        }else{
+            user.create({
+                nickname : new_nickname,
+                email : new_email,
+                password : new_passowrd
+            }).then(result => {
+                res.status(201);
+                res.send(result.dataValues);
+            })
+        }
+    }).catch(err => {
+        res.status(404).send(err);
     })
-    
-    if(newUser !== null){
-        // 닉네임, 이메일 분기 필요!
-        res.status(409);
-        res.send("already exist!")
-    }else{
-        await user.create({
-            nickname : new_nickname,
-            email : new_email,
-            password : new_passowrd
-        }).then(result => {
-            res.status(201);
-            res.send(result.dataValues);
-        })
-    }
-
   },
 };
