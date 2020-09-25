feb69a31b39c96ecabf95dc2a94f36a08237ab26
bapbodanbbang <62498196+bapbodanbbang@users.noreply.github.com>
2020-09-25 13:39:17 +0900

    [ADD] alarm API - delete, get, post

--- controller/user/signup.js
+++ controller/user/signup.js
@@ -2,32 +2,36 @@ const { user, Sequelize } = require('../../models');
 const OP = Sequelize.Op;
 
 module.exports = {
+
   post: async (req, res) => {
-    let new_nickname = req.body.nickname;
-    let new_email = req.body.email;
-    let new_passowrd = req.body.email;
+      const { email, nickname, password } = req.body;
 
     // 이메일이나 닉네임이 동일한 것이 있을 때 에러!
-    await user.findOne({
-        where : {
-            [OP.or] : [{nickname : new_nickname}, {email : new_email}]
-        }
-    }).then(result => {
-        if(result !== null){
-            res.status(409);
-            res.send("already exist!")
-        }else{
-            user.create({
-                nickname : new_nickname,
-                email : new_email,
-                password : new_passowrd
-            }).then(result => {
-                res.status(201);
-                res.send(result.dataValues);
-            })
-        }
-    }).catch(err => {
-        res.status(404).send(err);
-    })
+    await user
+        .findOne({
+            where : {
+                nickname : nickname
+            }
+        })
+        .then(result => {
+            if(result !== null){
+                res.status(409);
+                res.send("already exist!")
+            }else{
+                async function createUser () {
+                    await user.create({
+                        nickname : nickname,
+                        email : email,
+                        password : password
+                    }).then(result => {
+                    res.status(201).json(result);
+                    })
+                }
+                createUser();
+            }
+        })
+        .catch(err => {
+            res.status(404).send(err);
+        })
   },
 };
