d9d19c130b259016f26357e21c0a0d85f520361c
chaerynny <ryn3777@gmail.com>
2020-09-26 20:19:45 +0900

    [UPDATE]signout api

--- controller/user/signout.js
+++ controller/user/signout.js
@@ -2,11 +2,7 @@ const { user } = require('../../models');
 
 module.exports = {
   post: async (req, res) => {
-    const sess = req.session;
-    if(sess.userid) {
-      req.session.destroy(err => {
-        console.log(err)
-      })
-    }
+    req.session.destory() // 세션 삭제
+    res.clearCookie('connect.sid') // 세션 쿠키 삭제
   },
 };
