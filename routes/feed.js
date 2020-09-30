4b6fe126087e1128747d7481afa422ce94265c1b
민재 KIM <62498196+bapbodanbbang@users.noreply.github.com>
2020-09-30 16:22:08 +0900

    Update feed.js

--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const MyFeed = require('../controller/feed/Feed');
-const HomeFeed = require('../controller/feed/HomeFeed');
-const MakeFeed = require('../controller/feed/MakeFeed');
+const MyFeed = require('../controller/feed/feed');
+const HomeFeed = require('../controller/feed/homefeed');
+const MakeFeed = require('../controller/feed/makefeed');
 
 router.get('/myfeed/:id', MyFeed.getmyfeed);
 router.post('/makefeed/:id', MakeFeed.makefeed);
