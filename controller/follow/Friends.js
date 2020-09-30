a4bb3527f4f32faf87aaa561854c872bd4cbeccb
chaerynny <ryn3777@gmail.com>
2020-09-30 17:49:55 +0900

    [update] fix follow ing...

--- controller/follow/Friends.js
+++ controller/follow/Friends.js
@@ -1,4 +1,5 @@
 const { follow } = require('../../models');
+const { user } = require('../../models');
 
 /*
 1. follow 테이블에서 my_id에 해당하는것을 모두 찾는다.
@@ -9,12 +10,23 @@ module.exports = {
   get: async (req, res) => {
     const user_id = req.params.id; // 본인(나)
 
-    await follow.findAll({
+    const friends = await follow.findAll({
       where: {
         my_id: user_id
       }
-    }).then(result => {
-      res.status(200).json(result);
-    }).catch(err => res.sendStatus(404));
+    })
+    // .then(result => {
+    //   res.status(200).json(result);
+    // }).catch(err => res.sendStatus(404));
+
+    await friends.forEach(friend => {
+      user.findOne({
+        where: {
+          id: friend.dataValues.friend_id
+        }
+      }).then(result => {
+        res.status(200).json(result);
+      }).catch(err => res.status(404).send(err));
+    })
   }
 }
