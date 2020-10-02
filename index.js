fccd1cc18d095349c4010eb001f46c72110d15ba
민재 KIM <62498196+bapbodanbbang@users.noreply.github.com>
2020-10-02 18:49:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,63 +9,13 @@ const port = process.env.SERVER_PORT || 3000;
 const missionRouter = require('./routes/mission');
 const userRouter = require('./routes/user');
 const feedRouter = require('./routes/feed');
+const passport = require("passport");
+const passportConfig = require("./controller/user/passport.js");
 const alarmRouter = require('./routes/alarm');
 const followRouter = require('./routes/follow')
-
-
-//test google
-const { google } = require('googleapis');
-var googleClient = require('../im22project5-server/config/google.json');
- 
-const googleConfig = {
-  clientId: googleClient.web.client_id,
-  clientSecret: googleClient.web.client_secret,
-  redirect: googleClient.web.redirect_uris[0]
-};
- 
-const oauth2Client =new google.auth.OAuth2(
-  googleConfig.clientId,
-  googleConfig.clientSecret,
-  googleConfig.redirect
-);
-
-const url = oauth2Client.generateAuthUrl({
-  access_type:'offline',  
-  scope: 'https://www.googleapis.com/auth/plus.me'
-});
- 
-function getGooglePlusApi(auth) {
-  return google.plus({ version:'v1', auth });
-}
-
-async function googleLogin(code) {
-  const { tokens } = await oauth2Client.getToken(code);
-  oauth2Client.setCredentials(tokens);
-  oauth2Client.on('tokens', (token) => {
-    if(tokens.refresh_token){
-      console.log("리프레시 토큰 :", tokens.refresh_token);
-    }
-    console.log("액세스 토큰:", tokens.access_token);
-  });
-  const plus = getGooglePlusApi(oauth2Client);
-  const res = await plus.people.get({ userId:'me' });
-  console.log(`Hello ${res.data.displayName}! ${res.data.id}`);
-  return res.data.displayName;
-}
-
-app.get('/login',function (req, res) {
-  res.redirect(url);
-});
- 
-app.get("/auth/google/callback", async function (req, res) {
- 
-  const displayName = await googleLogin(req.query.code);
-  console.log(displayName);
- 
-  res.redirect("http://get-up-mate.s3-website.ap-northeast-2.amazonaws.com");
-});
-
-
+passportConfig();
+dotenv.config();
+​
 //test session sustain
 const mysqlStore = require('express-mysql-session')(session);
 const options = {
@@ -76,7 +26,7 @@ const options = {
   database : 'getupmate'
 };
 const sessionStorage = new mysqlStore(options);
-
+​
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors( { origin : '*' } ));
@@ -95,16 +45,17 @@ app.use(
     // --end
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/follow', followRouter);
 app.use('/mission', missionRouter);
 app.use('/user', userRouter);
 app.use('/alarm', alarmRouter);
 app.use('/feed', feedRouter);
-// app.use("/auth", require("./routes/user"));
+app.use("/auth", require("./routes/user"));
 app.use('/upload', express.static(__dirname+'/uploads/images'));
 app.use('/img', express.static(__dirname+'/uploads/images'));
-dotenv.config();
-
+​
 // 구글 로그인 테스트를 위한 index.html 랜더링(client에서 버튼 만들어지면 없앨 예정)
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
@@ -112,7 +63,7 @@ dotenv.config();
 // app.get('/',function(req,res){
 //   res.render('index.html');
 // });
-
+​
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -120,5 +71,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
 })
-
+​
 module.exports = app;
